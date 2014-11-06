fb88ba33ab52a5484c85f48fc37e7cf79e04118c
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2014-11-06 14:49:34 -0800

    not working rook

--- src/solvers.js
+++ src/solvers.js
@@ -33,7 +33,7 @@ window.findNRooksSolution = function(n) {
       }
     }
   }
-  var solution = board.getBoardMatrix(); //fixme
+  var solution = board.getBoardMatrix(); 
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -43,7 +43,25 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; 
+
+  function subroutine (n, board) {
+  // start with a fresh board if current board is not passed
+  var board = board || new Board({n:n});
+  var rowsIndeces = _.range(n);
+  for (var i = 0; i <= rows; i++) {
+    if (board.)
+  };
+  // if all rows contain a piece each
+    // solutionCount++
+    // return
+
+  // recurse through unconfilcting rows
+    // find first unconflicting row in board
+    // make copies of the board for every unconflicting pos in row
+    // place piece at first unconflicting piece
+
+  }(n);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
